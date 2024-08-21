f9c5321023f84ad989fe6fb56ec6e162ad5bb484
Suraj Kumar <146581639+surajisacoder@users.noreply.github.com>
2024-08-22 00:05:51 +0530

    Update script.js

--- script.js
+++ script.js
@@ -80,19 +80,17 @@ const songData =[
         source:"songs/ecstacy-slowed-ecstacy-slowed.mp3"
     },
 ]; 
+
+
 const toggler = document.querySelector(".switch");
 const checkboxEl = document.querySelector(".check-box");
-const songQueueEle = document.querySelector(".song-queue");
-const songImageEle = document.querySelector(".song-img");
-const songNameEle = document.querySelector(".song-name");
-const artistNameEle = document.querySelector(".artist-name");
-const audioEle = document.querySelector(".audio-controls");
-const cardEle = document.querySelectorAll(".card");
-const songInfoEle = document.querySelector(".song-info-container");
-const bodyEl = document.querySelector("body");
-const buttonEle = document.querySelectorAll(".btn");
-const searchEle = document.querySelector(".search-bar");
 const playListElement = document.querySelectorAll(".playlist");
+const cardEle = document.querySelectorAll(".card");
+const buttonEle = document.querySelectorAll(".btn");
+const songInfoEle = document.querySelector(".song-info-container");
+const searchEle = document.querySelector(".search-bar");
+const bodyEl = document.querySelector("body");
+
 toggler.addEventListener('click',()=>{
     if(!checkboxEl.checked){
         checkboxEl.checked = true;
@@ -102,62 +100,75 @@ toggler.addEventListener('click',()=>{
         checkboxEl.checked = false;
         toggleLightTheme();
     }
-});
+}); 
+  
 function toggleDarkTheme(){
     const songEles = document.querySelectorAll(".song");
+     
     songEles.forEach((ele)=>{ 
-        ele.style.backgroundColor = "#64696C"; 
-    });
-    console.log(playListElement);
-    playListElement.forEach((ele)=>{
-        console.log(ele);
         ele.style.backgroundColor = "#64696C";
     });
+
+    console.log(playListElement);
+    playListElement.forEach((ele)=>{
+        console.log(ele); 
+        ele.style.backgroundColor = "#64696C";
+    });
+
     cardEle.forEach((card)=>{
         card.style.backgroundColor = "#263238";
         card.style.boxShadow = "-1px -1px 5px 0.1px #64696C";
     });
+
     buttonEle.forEach((button)=>{
         button.style.backgroundColor = "darkviolet";
     });
+
     bodyEl.style.backgroundColor = "#565657";
     bodyEl.style.color = "white";
     songInfoEle.style.backgroundColor = "#64696C";
     songInfoEle.style.boxShadow = "-1px -1px 5px 0.1px #64696C";
-    searchEle.style.backgroundColor = "#263238";
     searchEle.style.backgroundColor = "white";
 }
+
 function toggleLightTheme(){
     const songEles = document.querySelectorAll(".song");
+
     songEles.forEach((ele)=>{
         ele.style.backgroundColor = "#0b81bc";
     });
+
     cardEle.forEach((card)=>{
         card.style.backgroundColor = "#6bb7de";
         card.style.boxShadow = "-1px -1px 5px 0.1px purple";
     });
+
     buttonEle.forEach((button)=>{
         button.style.backgroundColor = "blue";
     });
+
     bodyEl.style.backgroundColor = "lightCyan";
     bodyEl.style.color = "black";
     songInfoEle.style.backgroundColor = "#0573ab";
     songInfoEle.style.boxShadow = "-1px -1px 5px 0.1px purple";
     searchEle.style.backgroundColor = "white";
 }
+
 const dropdownEle = document.querySelector("#filter-selection");
 const genre = ["All", "Pop", "Rock", "Hip Hop"]
 genre.forEach((option)=>{
     const optionEle = document.createElement("option");
     optionEle.text = option;
     optionEle.value = option;
-    optionEle.classList.add("genre-option");
-    dropdownEle.appendChild(optionEle);
+    optionEle.classList.add("genre-option"); 
+    dropdownEle.appendChild(optionEle); 
 });
 dropdownEle.addEventListener('change', ()=>{
     showSongs();  
-});
-function showSongs(){                   
+}); 
+
+const songQueueEle = document.querySelector(".song-queue");
+function showSongs(){                      
     songQueueEle.textContent = "";
     if(dropdownEle.value == "All"){
         songData.forEach((song)=>{
@@ -166,7 +177,6 @@ function showSongs(){
     }
     else{
         songData.forEach((song)=>{
-            console.log();
             if(dropdownEle.value === song.genre){
                 createSong(song);
             }
@@ -174,15 +184,16 @@ function showSongs(){
     }  
 }
 showSongs();
+
 const searchSongEle = document.querySelector(".song-search");
 const searchBtnEle = document.querySelector("#song-search-btn");
 const clearSearchEle = document.querySelector("#close-btn");
 searchSongEle.addEventListener('submit', (event)=>{
-    event.preventDefault();
+    event.preventDefault(); 
     const searchBarEle = document.querySelector("#song-search-bar");
-    const searchedSong = searchBarEle.value;
-    const songQueue = Array.from(songQueueEle.children);
-    songFoundEle = songQueue.find((songEle)=> { 
+    const searchedSong = searchBarEle.value; 
+    const songQueue = Array.from(songQueueEle.children);  
+    songFoundEle = songQueue.find((songEle)=> {     
         return songEle.textContent.toLowerCase() === searchedSong.toLowerCase();
     });
     searchBtnEle.style.display = "none";
@@ -194,6 +205,7 @@ searchSongEle.addEventListener('submit', (event)=>{
     songQueueEle.textContent = "";
     songQueueEle.appendChild(songFoundEle);
 });
+
 clearSearchEle.addEventListener('click', clearSearch);
 function clearSearch(){ 
     searchBtnEle.style.display = "inline-block";
@@ -202,25 +214,32 @@ function clearSearch(){
     document.querySelector("#song-search-bar").text = "";
     document.querySelector("#song-search-bar").value = "";
 }
+
 let currentSongIndex = 0;
 let currentSong = songData[currentSongIndex];
 let currentSongEle = document.querySelector(".song"); 
 currentSongEle.style.backgroundColor = "#03557e";
+
 function createSong(song){
-    const songElement = document.createElement("div");
-    songElement.classList.add("song");
+    const songElement = document.createElement("div");  
+    songElement.classList.add("song"); 
     songElement.id = (`s${song.id}`);
     songElement.textContent = song.name;
     songQueueEle.appendChild(songElement);
     songElement.addEventListener('click', ()=>{ 
         currentSongEle.style.backgroundColor = (checkboxEl.checked)?"#64696C":"#0b81bc";
-        currentSongIndex = songData.indexOf(song);
+        currentSongIndex = songData.indexOf(song); 
         currentSong = song;
-        currentSongEle = songElement;
+        currentSongEle = songElement; 
         // audioEle.autoplay = true;
         renderCurrentSong();
     });
-}
+}       
+
+const songImageEle = document.querySelector(".song-img");
+const songNameEle = document.querySelector(".song-name");
+const artistNameEle = document.querySelector(".artist-name");
+const audioEle = document.querySelector(".audio-controls")
 function renderCurrentSong(){
     songImageEle.src = currentSong.img;
     songNameEle.textContent = currentSong.name;
@@ -229,6 +248,7 @@ function renderCurrentSong(){
     audioEle.autoplay = true;
     currentSongEle.style.backgroundColor = "#03557e";
 }
+
 let songQueue = Array.from(songQueueEle.children); 
 const prevButtonEle = document.querySelector("#prev"); 
 prevButtonEle.addEventListener('click', ()=>{
@@ -239,7 +259,8 @@ prevButtonEle.addEventListener('click', ()=>{
         index = songQueue.length-1;
     }
     selectSong(index);
-});
+}); 
+
 const nextButtonEle = document.querySelector("#next");
 nextButtonEle.addEventListener('click', ()=>{
     songQueue = Array.from(songQueueEle.children);
@@ -250,26 +271,29 @@ nextButtonEle.addEventListener('click', ()=>{
     }
     selectSong(index);
 }); 
+  
 function selectSong(index){ 
     currentSongEle.style.backgroundColor = (checkboxEl.checked)?"#64696C":"#0b81bc";
     currentSongEle = songQueue[index];
-    currentSongIndex = parseInt(currentSongEle.id.slice(1))-1;
+    currentSongIndex = parseInt(currentSongEle.id.slice(1))-1; 
     currentSong = songData[currentSongIndex];  
     renderCurrentSong();
-} 
-let playList = {};
+}    
+
+let playList = {};     
 let currentPlaylist = new Set();   
 const playListBtn = document.querySelector("#playlist-btn");
 const currPlaylistEle = document.querySelector(".current-playlist");
 const allPlaylistEle = document.querySelector(".all-playlist");
 const playlistInputEle = document.querySelector("#playlist-input");
 const formEle = document.querySelector(".search-container");
+
 formEle.addEventListener('submit', createPlayList);
 function createPlayList(event){
-    event.preventDefault();
-    const playListName = playlistInputEle.value;
-    playlistInputEle.text = "";
-    playlistInputEle.value = "";
+    event.preventDefault();   
+    const playListName = playlistInputEle.value; 
+    playlistInputEle.text = "";                  
+    playlistInputEle.value = "";   
     if(playListName in playList){
         alert("The playlist name already exists, create playlist with new name")
         return;
@@ -278,6 +302,7 @@ function createPlayList(event){
     currentPlaylist = playList[`${playListName}`]; 
     renderAllPlaylist(); 
 } 
+
 function renderAllPlaylist(){
     allPlaylistEle.textContent = ""; 
     for(let playListName of Object.keys(playList)){   
@@ -287,37 +312,39 @@ function renderAllPlaylist(){
         playListEle.classList.add("playlist");
         playListEle.style.backgroundColor = (checkboxEl.checked)?"#64696C":"#0b81bc";
         allPlaylistEle.appendChild(playListEle);     
-        renderPlaylist();       
+        renderPlaylist();        
         playListEle.addEventListener('click', ()=>{     
             playListEle.style.backgroundColor = (checkboxEl.checked)?"#64696C":"#0b81bc";
             currentPlaylist = playList[`${playListName}`]; 
-            renderPlaylist(); 
+            renderPlaylist();  
         });
-    } 
-}
+    }  
+} 
+
 playListBtn.addEventListener('click', ()=>{ 
     if(Object.keys(playList).length === 0){
         alert("Create a playlist to add song");
-        return; 
-    }
+        return;  
+    } 
     currentPlaylist.add(currentSong);
     renderPlaylist();
 });
+
 function renderPlaylist(){  
-    currPlaylistEle.textContent = ""; 
+    currPlaylistEle.textContent = "";                                                              
     currentPlaylist.forEach((song)=>{  
         const songElement = document.createElement("div");  
         songElement.classList.add("song");
         songElement.textContent = song.name;
         currPlaylistEle.appendChild(songElement);
         songElement.style.backgroundColor = (checkboxEl.checked)?"#64696C":"#0b81bc"; 
-        const deleteButton = document.createElement("button");
+        const deleteButton = document.createElement("button"); 
         deleteButton.classList.add("delete-btn");
-        deleteButton.textContent = "X";
+        deleteButton.textContent = "X";    
         songElement.appendChild(deleteButton); 
         deleteButton.addEventListener('click', ()=>{ 
             songElement.remove();
-            currentPlaylist.delete(song);
+            currentPlaylist.delete(song);   
             console.log(currentPlaylist);
         });
         songElement.addEventListener('click', ()=>{
@@ -330,6 +357,7 @@ function renderPlaylist(){
         });
     });
 }
+
 const searchPlaylistEle = document.querySelector(".playlist-search");
 const searchPlaylistBarEle = document.querySelector("#playlist-search-bar");
 const searchPlaylistBtnEle = document.querySelector("#playlist-search-btn");
@@ -337,7 +365,7 @@ const clearPlaylistSearchEle = document.querySelector("#playlist-close-btn");
 searchPlaylistEle.addEventListener('submit', (event)=>{
     event.preventDefault();
     const searchedPlaylist = searchPlaylistBarEle.value;
-    const PlaylistQueue = Array.from(allPlaylistEle.children);
+    const PlaylistQueue = Array.from(allPlaylistEle.children);   
     playlistFoundEle = PlaylistQueue.find((playListEle)=> {
         return playListEle.textContent === searchedPlaylist;
     });
@@ -348,8 +376,9 @@ searchPlaylistEle.addEventListener('submit', (event)=>{
         return;
     }
     allPlaylistEle.textContent = "";
-    allPlaylistEle.appendChild(playlistFoundEle);
+    allPlaylistEle.appendChild(playlistFoundEle);  
 });
+
 clearPlaylistSearchEle.addEventListener('click', clearPlaylistSearch);
 function clearPlaylistSearch(){
     searchPlaylistBtnEle.style.display = "inline-block";
